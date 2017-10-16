d27fb728f190250cf1a044abde3e7acaf612336b
davidryv <davidryv@outlook.com>
2017-10-16 00:07:03 -0500

    add config

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
+import os
+
 class config:
     def __init__(self):
-        self.admin_username = request.args.get('admin_username')
-
-        self.admin_apikey = request.args.get('admin_apikey')
-
-        self.twilio_account_sid = request.args.get('twilio_account_sid')
-
-        self.twilio_auth_token = request.args.get('twilio_auth_token')
-
-        self.twilio_phone_number = request.args.get('twilio_phone_number')
+        read = open("credentials.txt", "r")
+        for line in read:
+            if('admin_username' in line):
+                self.admin_username = line.split("=",1)[1].replace("\n", "")
+                print(self.admin_username)
+            elif('admin_apikey' in line):
+                 self.admin_apikey = line.split("=",1)[1].replace("\n", "")
+                 print(self.admin_apikey)
+            elif('twilio_account_sid' in line):
+                self.twilio_account_sid = line.split("=",1)[1].replace("\n", "")
+                print(self.twilio_account_sid)
+            elif('twilio_auth_token' in line):
+                self.twilio_auth_token = line.split("=",1)[1].replace("\n", "")
+                print(self.twilio_auth_token)
+            elif('twilio_phone_number' in line):
+                self.twilio_phone_number = line.split("=",1)[1].replace("\n", "")
+                print(self.twilio_phone_number)
+        read.close()
